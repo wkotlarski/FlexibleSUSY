b58ac0d784096285311a2b9949d59c6063cd7f3f
Kien Dang Tran <kiendangtran0@gmail.com>
2019-01-25 15:13:33 +0100

    NPointFunctions: use softsusy::d27 for d00 since they are equivalent for vanishing momenta.

--- meta/NPointFunctions.m
+++ meta/NPointFunctions.m
@@ -350,8 +350,8 @@ CreateCXXFunctions[nPointFunctions_List, names_List,
     loopFunctionRules = Switch[OptionValue[LoopFunctions],
       "LoopTools", {},
       "FlexibleSUSY",
-         Print["Warning: Using FlexibleSUSY loop functions will only remap A0, B0, C0 and D0."];
-         Print["Warning: FlexibleSUSY loop functions C0 and D0 require zero external momenta."];
+         Print["Warning: Using FlexibleSUSY loop functions will only remap A0, B0, C0, D0 and D00."];
+         Print["Warning: FlexibleSUSY loop functions C0, D0 and D00 require zero external momenta."];
          {
            LoopTools`A0i[LoopTools`aa0, args__] :> "softsusy::a0"[Sequence @@ ("std::sqrt" /@ List[args]),
                                              "context.scale()"],
@@ -360,7 +360,8 @@ CreateCXXFunctions[nPointFunctions_List, names_List,
            LoopTools`B0i[LoopTools`bb0, args__] :> "softsusy::b0"[Sequence @@ ("std::sqrt" /@ List[args]),
                                              "context.scale()"],
            LoopTools`C0i[LoopTools`cc0, 0, 0, 0, args__] :> "softsusy::c0"[Sequence @@ ("std::sqrt" /@ List[args])],
-           LoopTools`D0i[LoopTools`dd0, 0, 0, 0, 0, 0, 0, args__] :> "softsusy::d0"[Sequence @@ ("std::sqrt" /@ List[args])]
+           LoopTools`D0i[LoopTools`dd0, 0, 0, 0, 0, 0, 0, args__] :> "softsusy::d0"[Sequence @@ ("std::sqrt" /@ List[args])],
+           LoopTools`D0i[LoopTools`dd00, 0, 0, 0, 0, 0, 0, args__] :> "softsusy::d27"[Sequence @@ (Map[Sqrt, List[args]] /. Sqrt[(x___)^2] :> x)]
          },
        _, Return["Option LoopFunctions must be either LoopTools or FlexibleSUSY"]];
 
