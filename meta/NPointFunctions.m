6df68137d77027fb298333dfa84f31f2535b806b
Kien Dang Tran <kiendangtran0@gmail.com>
2019-03-26 14:49:26 +0100

    NPointFunctions: check whether the external fields are scalar/fermion

--- meta/NPointFunctions.m
+++ meta/NPointFunctions.m
@@ -173,10 +173,11 @@ be either True or False"];
         "NPointFunctions`NPointFunction[]: Option OnShellFlag must be either \
         True or False."];
 
-    (*Utils`AssertWithMessage[And @@
-			TreeMasses`IsScalar /@ Join[inFields, outFields],
-			"NPointFunctions`NPointFunction[]: Only external scalars are \
-supported (for now)."];*)
+    Utils`AssertWithMessage[And @@
+			TreeMasses`IsScalar /@ Join[inFields, outFields] ||
+            And @@ TreeMasses`IsFermion /@ Join[inFields, outFields],
+			"NPointFunctions`NPointFunction[]: Only external scalars/fermions are \
+supported (for now)."];
 
     inFields = Vertices`StripFieldIndices[inFields];
     outFields = Vertices`StripFieldIndices[outFields];
